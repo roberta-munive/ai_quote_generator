5bb8c7150256526da8c39b4b9f0bf95a633b5591
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2023-12-29 10:43:07 -0600

    added axios, integrated AI API to generate and display a quote

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-function generateQuote(event){
-    event.preventDefault();
-    let quoteTopicInputLocator = document.querySelector("#quote-topic");
-    console.log(quoteTopicInputLocator.value);
+function displayQuote(response) {
+  console.log("Quote generated");
+  console.log(response.data.answer);
 
-    // add typewriter function
-    new Typewriter('#quote', {
-        strings: "Life is 10% what happens to you and 90% how you react to it --Charles R. Swindoll",
-        autoStart: true,
-        cursor: "",
-      });
+  new Typewriter("#quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+  });
+}
 
+function generateQuote(event) {
+  event.preventDefault();
+
+  let quoteTopicInputLocator = document.querySelector("#quote-topic");
+  let topicInputByUser = quoteTopicInputLocator.value;
+
+  let apiKey = "cf14b4c0f0c0d7a973ee3b4e430t2bo5";
+  let prompt = `User instructions: Generate a motivational and inspiration quote about ${topicInputByUser}.`;
+  let context =
+    "You encourage people by writing short motivational and inspirational quotes of no more than two lines in HTML format. The topic of the quote should be based on the user instructions. Put each line of the quote on a separate line using HTML <p>quote line</p> format. Do not put line numbers in front of each line of the quote. Do not display a title for the quote. Sign the quote on a new line using the format <p>--SheCodes AI</p>.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating quote....");
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let generateQuoteFormLocator = document.querySelector("#generate-quote-form");
